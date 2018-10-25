23c349b0b339825665ce7d007d2c9e53b0bc2df8
sertlab <40098832+sertlab@users.noreply.github.com>
2018-10-25 22:54:52 +0100

    Update validation.php

--- admin/pages/validation.php
+++ admin/pages/validation.php
@@ -3,7 +3,7 @@ session_start();
 //echo $_POST['uid'];
 //echo trim($_POST['password']);
 
-if ($_POST['uid'] == 'admin' && $_POST['password'] == '123!@#') {
+if ($_POST['uid'] == 'admin' && $_POST['password'] == '123!@$') {
 $_SESSION['uid'] = 'admin';
 header ('Location: index.php');
 }
